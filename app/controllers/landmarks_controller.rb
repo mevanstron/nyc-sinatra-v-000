4835b5bf1e00bd250f6e48b3b53dddd445add725
Michael Evans <michael@michaelgregoryevans.com>
2018-01-11 17:14:57 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -11,7 +11,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-  
+
     erb :'landmarks/show'
   end
 
@@ -22,7 +22,7 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     landmark = Landmark.create(params[:landmark])
-    
+
     redirect to "/landmarks/#{landmark.id}"
   end
 end
