917938b4de74fc73b4c1a1d79433ff97f5aece14
Michael Evans <michael@michaelgregoryevans.com>
2018-01-11 16:37:03 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,4 +3,10 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'landmarks/index'
   end
+
+  get '/landmarks/new' do
+    @figures = figure.all
+    @titles = Title.all
+    erb :'/landmarks/new'
+  end
 end
