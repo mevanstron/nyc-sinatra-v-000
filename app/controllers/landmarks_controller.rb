80b71c8ee6e2a74a9f3676972b77f039b6e7f324
Michael Evans <michael@michaelgregoryevans.com>
2018-01-11 17:03:44 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -17,7 +17,7 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     landmark = Landmark.create(params[:landmark])
-    binding.pry
+    #binding.pry
     redirect to "/landmarks/#{landmark.id}"
   end
 end
